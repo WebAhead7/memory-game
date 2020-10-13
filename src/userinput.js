d2595372916604063f8f8aea90bb7e1167aa9591
nizarhalloun <nshalloun@hotmail.com>
2020-10-13 20:55:15 +0300

    added event listener

--- src/userinput.js
+++ src/userinput.js
@@ -1,2 +1,19 @@
 const input = document.querySelector("input");
-input.addEventListener("keypress", (e) => {});
+input.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    const input = document.querySelector("input");
+    window.localStorage.setItem(input.value, 0);
+  }
+});
+
+function makeList() {
+  for (key in window.localStorage) {
+    console.log(key);
+  }
+}
+
+function clearGameHistory() {
+  localStorage.clear();
+}
+
+clearGameHistory();
