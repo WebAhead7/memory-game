d367b463362eb4a464f54976faedd3aec363bc88
nizarhalloun <nshalloun@hotmail.com>
2020-10-14 00:47:24 +0300

    added sorted list

--- src/userinput.js
+++ src/userinput.js
@@ -1,37 +1,53 @@
-const input = document.querySelector("input");
-input.addEventListener("keypress", (e) => {
-  if (e.key === "Enter") {
+class UserInput {
+  constructor() {
+    this.makeList();
+    this.listenToInput();
+  }
+  clearGameHistory() {
+    localStorage.clear();
+  }
+  makeList() {
+    if (!window.localStorage.getItem("logArr")) {
+      window.localStorage.setItem("logArr", JSON.stringify([]));
+    }
+    const list = document.createElement("ul");
+    const logArr = JSON.parse(window.localStorage.getItem("logArr"));
+    logArr
+      .sort((playerOne, playerTwo) => {
+        return playerOne.score > playerTwo.score ? -1 : 1;
+      })
+      .forEach((player, index) => {
+        const listElem = document.createElement("li");
+        listElem.textContent = `#${index + 1} playe name : ${
+          player.name
+        } \n score : ${player.score}`;
+        list.appendChild(listElem);
+      });
+    document.querySelector("body").appendChild(list);
+  }
+
+  listenToInput() {
     const input = document.querySelector("input");
-    window.localStorage.setItem(input.value, 5);
-  }
-});
-
-function makeList() {
-  const list = document.createElement("ul");
-  document.querySelector("body").appendChild(list);
-
-  for (key in window.localStorage) {
-    const listElem = document.createElement("li");
-    listElem.textContent = `playe name : ${key} \n score : ${window.localStorage.getItem(
-      key
-    )}`;
-    list.appendChild(listElem);
+    input.addEventListener(
+      "keypress",
+      (e) => {
+        if (e.key === "Enter") {
+          const name = input.value;
+          const logArr = JSON.parse(window.localStorage.getItem("logArr"));
+          logArr.push({ name: name, score: 4 });
+          window.localStorage.setItem("logArr", JSON.stringify(logArr));
+        }
+      },
+      false
+    );
   }
 }
 
-function clearGameHistory() {
-  localStorage.clear();
-}
-
-clearGameHistory();
-console.log(window.localStorage);
-makeList();
-
-
+new UserInput();
 
 // muhammad adding audio:
-window.addEventListener("DOMContentLoaded", event => {
-  const audio = document.querySelector("audio");
-  audio.volume = 0.2;
-  audio.play();
-});
+// window.addEventListener("load", (event) => {
+//   const audio = document.querySelector("audio");
+//   audio.volume = 0.2;
+//   audio.play();
+// });
