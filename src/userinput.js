588f4c657170c8a7ae8b8d3bcfb6c96a0338b352
nizarhalloun <nshalloun@hotmail.com>
2020-10-13 21:29:03 +0300

    added list

--- src/userinput.js
+++ src/userinput.js
@@ -2,13 +2,20 @@ const input = document.querySelector("input");
 input.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
     const input = document.querySelector("input");
-    window.localStorage.setItem(input.value, 0);
+    window.localStorage.setItem(input.value, 5);
   }
 });
 
 function makeList() {
+  const list = document.createElement("ul");
+  document.querySelector("body").appendChild(list);
+
   for (key in window.localStorage) {
-    console.log(key);
+    const listElem = document.createElement("li");
+    listElem.textContent = `playe name : ${key} \n score : ${window.localStorage.getItem(
+      key
+    )}`;
+    list.appendChild(listElem);
   }
 }
 
@@ -17,3 +24,5 @@ function clearGameHistory() {
 }
 
 clearGameHistory();
+console.log(window.localStorage);
+makeList();
